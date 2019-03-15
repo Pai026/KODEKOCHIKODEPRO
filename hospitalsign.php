a189dd405707e702f9b0f65f1e2d61ac371f5162
Abhiram Pai <36255896+Pai026@users.noreply.github.com>
2019-03-15 17:22:20 +0530

    Add files via upload

--- hospitalsign.php
+++ hospitalsign.php
@@ -10,8 +10,8 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn_error);
 }
 
-$stmt = $conn->prepare("INSERT INTO hospital (Hospital_name,Haddress,Hphno) VALUES (?,?,?)");
-$stmt->bind_param("ssi", $_POST['Hospital_name'], $_POST['Haddress'],$_POST['Hphone']);
+$stmt = $conn->prepare("INSERT INTO hospital (Hospital_name,Haddress,Hphno,Hpass) VALUES (?,?,?,?)");
+$stmt->bind_param("ssis", $_POST['Hospital_name'], $_POST['Haddress'],$_POST['Hphone'],$_POST['Pass']);
 $stmt->execute();
 
 echo "signup successfull";
