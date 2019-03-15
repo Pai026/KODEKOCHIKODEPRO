de3f360b9bbea0bfe9c37713e43d82146896928d
donacharles <36255899+donacharles@users.noreply.github.com>
2019-03-15 17:30:51 +0530

    Add files via upload

---
+++ sign_up.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "app";
+
+$conn = new \MySQLi($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$stmt = $conn->prepare("INSERT INTO outpatient(OPname,OPphno,OPemail,OPaddress,OPpass) VALUES (?,?,?,?,?)");
+$stmt->bind_param("sisss" , $_POST['OPname'], $_POST['OPphno'], $_POST['OPemail'], $_POST['OPaddress'], $_POST['OPpass']) ;
+$stmt->execute();
+
+echo"SIGN_UP SUCCESSFUL";
+$stmt->close();
+$conn->close();
+?>
