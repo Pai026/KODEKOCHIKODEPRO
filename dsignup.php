ee3d7a0a0640916b39cf5b78f2100b234e04250a
Abhiram Pai <36255896+Pai026@users.noreply.github.com>
2019-03-15 23:29:18 +0530

    Add files via upload

--- dsignup.php
+++ dsignup.php
@@ -10,8 +10,8 @@ if($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
 }
 
-$stmt = $conn->prepare("INSERT INTO driver (Dname,LNO,Dpass,Dphno,Vno) VALUES (?,?,?,?,?)");
-$stmt->bind_param("sisis", $_POST['Dname'],$_POST['LNO'],$_POST['Dpass'],$_POST['Dphno'],$_POST['Vno']);
+$stmt = $conn->prepare("INSERT INTO driver (Dname,LNO,Dpass,Dphno,Vno,Demail) VALUES (?,?,?,?,?,?)");
+$stmt->bind_param("sisiss", $_POST['Dname'],$_POST['LNO'],$_POST['Dpass'],$_POST['Dphno'],$_POST['Vno'],$_POST['Demail']);
 $stmt->execute();
 
 echo "Signup Successfull";
